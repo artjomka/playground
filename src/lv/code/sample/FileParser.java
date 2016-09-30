9cad64d4eb67015d37b83ad1faef3a0b7d568fdf
Artjom Kalita <aka@ambita.com>
2016-09-30 10:12:05 +0300

    Small package changes to hide its true meaning

--- src/lv/code/sample/FileParser.java
+++ src/lv/code/sample/FileParser.java
@@ -12,47 +12,64 @@ import java.util.List;
  */
 public class FileParser {
 
-    private static int doItCount = 0;
+  private static int doItCount = 0;
 
-    public ArrayList<ParseResult> doIt(String x){
-        try {
-            Path path = Paths.get(x);
-            ArrayList<ParseResult> parseResults = new ArrayList<>();
-            // Read lines from file
-            List<String> lines = Files.readAllLines(path);
-            // Iterate all lines
-            for (int i = 0; i < lines.size(); i++ ){
-                String value1 = "";
-                String value2 = "";
-                String value3 = "";
-                // Get line
-                String line = lines.get(i);
-                // Split line
-                String[] split = line.split("|");
-                // Loop
-                for (int iz = 0; iz < split.length; iz++) {
-                    if (split[iz].equals("value1")) {
-                        if (doItCount < 100) {
-                            value1 = split[iz];
-                        }
+  public ArrayList<ParseResult> doIt(String x) {
+    String m = new String("OK");
+    try {
 
-                    } else if (split[iz].equals("value2")) {
-                        System.out.print("Value 2 " + split[iz]);
-                        value2 = split[iz];
-                    } else if (split[iz].equals("hello")) {
-                        value3 = split[iz] + " 3 ";
-                        doItCount++;
-                    }
-
-                }
-                parseResults.add(new ParseResult(value1, value2, value3, "",""));
+      Path path = Paths.get(x);
+      ArrayList<ParseResult> parseResults = new ArrayList<>();
+      // Read lines from file
+      List<String> lines = Files.readAllLines(path);
+      // Iterate all lines
+      for (int i = 0; i < lines.size(); i++) {
+        String value1 = "";
+        String value2 = "";
+        String value3 = "";
+        // Get line
+        String line = lines.get(i);
+        // Split line
+        String[] split = line.split("|");
+        // Loop
+        for (int iz = 0; iz < split.length; iz++) {
+          if (split[iz].equals("value1")) {
+            if (doItCount < 100) {
+              value1 = split[iz];
             }
-        } catch (IOException e) {
+          }
+          else if (split[iz].equals("value2")) {
+            System.out.print("Value 2 " + split[iz]);
+            if (value1.length() > 5)
+              value2 = split[iz];
+            else
+            value2 = split[iz] +  " details";
+          }
+          else if (split[iz].equals("hello")) {
+            value3 = split[iz] + " 3 ";
+            doItCount++;
+          }
         }
-        return null;
+        parseResults.add(new ParseResult(value1, value2, value3, "", ""));
+      }
     }
+    catch (Exception e) {
+      if (e.getClass().getName().equals(IOException.class.getName())) {
+        m = "INPUT_EXCEPTION";
+      } else  {
+        m = "EXCEPTION";
+      }
+    }
+    System.out.println("Processing result status was" + m);
+    return null;
+  }
 
-    public static int getDoItCount() {
-        return doItCount;
-    }
+  public static int getDoItCount() {
+    return doItCount;
+  }
+
+  public static void main(String[] args) {
+    FileParser fileParser = new FileParser();
+    fileParser.doIt(args[0]);
+  }
 }
